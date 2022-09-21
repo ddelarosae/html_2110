652841f56f7f64de196368abc35965deda9e0bb1
ddelarosae <ddelarose@uninorte.edu.co>
2022-09-21 15:22:03 -0500

    sesion 7

--- sesion7/nombre.js
+++ sesion7/nombre.js
@@ -2,10 +2,12 @@ function validar_formulario(){
     var username = document.formRegistro.usuario;
     var email = document.formRegistro.correo;
     var password = document.formRegistro.pass;
+    var seleccionado=document.getElementById('condiciones').checked;
 
     var username_len=username.value.length;
     var password_len=password.value.length;
     var email_len=email.value.length;
+  
     expresion=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     
     if(username_len<8 || username_len==0){
@@ -16,5 +18,8 @@ function validar_formulario(){
     }
     if(email_len==0 || !email.value.match(expresion)){
         alert("Revisar correo");
+    }
+    if(!seleccionado){
+        alert("Se debe seleccionar las condiciones");
     }
 }
