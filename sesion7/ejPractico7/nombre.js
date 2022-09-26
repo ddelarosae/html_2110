a837e9358eb35239864c502d616bcefdadc74e2f
ddelarosae <ddelarose@uninorte.edu.co>
2022-09-26 13:04:33 -0500

    desarrollo de ejercicio practico 7

---
+++ sesion7/ejPractico7/nombre.js
@@ -0,0 +1,33 @@
+function validar_formulario(){
+    var username = document.formRegistro.usuario;
+    var email = document.formRegistro.correo;
+    var password = document.formRegistro.pass;
+    var seleccionado=document.getElementById('condiciones').checked;
+
+    var username_len=username.value.length;
+    var password_len=password.value.length;
+    var email_len=email.value.length;
+  
+    expresion=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    
+    if(username_len<8 || username_len==0){
+        alert("Se solicita ingresar el nombre de usuario");
+    }
+    if(password_len<8 || password_len==0){
+        alert("Se solicita ingresar el valor de password");
+    }
+    if(email_len==0 || !email.value.match(expresion)){
+        alert("Revisar correo");
+    }
+    if(!seleccionado){
+        alert("Se debe seleccionar las condiciones");
+    }
+}
+function mostrarPassword(obj){
+    var obj = document.getElementById("pass");
+    obj.type = "text";
+}
+function ocultarPassword(obj){
+    var obj = document.getElementById("pass");
+    obj.type = "password";
+}
